c1283d4bf3ae5cc00ef7ba2ca8a12ce872ba84f8
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-08 21:52:13 +0700

    create product category model

---
+++ PROJECT/app/Models/ProductCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    protected $table = 'product_category';
+    public $timestamps = false;
+}
