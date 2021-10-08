de1e354ef22c5f478d48f2b272e91e3f5223b536
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-09 00:18:17 +0700

    implement all routes requested

--- PROJECT/routes/web.php
+++ PROJECT/routes/web.php
@@ -22,4 +22,7 @@ Auth::routes();
 Route::middleware('auth')->group(function () {
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/users/{type?}', 'UserController@index')->name('users');
+    Route::get('/products', 'ProductController@index')->name('products');
+    Route::get('/orders', 'OrderController@index')->name('orders');
+    Route::get('/reports/{type?}', 'ReportController@index')->name('reports');
 });
