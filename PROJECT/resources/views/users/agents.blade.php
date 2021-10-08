448084f2ae1ec9bb19295eb95dfb691ce9ed4922
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-09 00:17:59 +0700

    modify agents view

--- PROJECT/resources/views/users/agents.blade.php
+++ PROJECT/resources/views/users/agents.blade.php
@@ -1,34 +1,52 @@
 
 <div>
-    <table class="table table-striped table">
-        <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th></th>
-        </tr>
-        @if(count($datas) == 0)
-        <tr>
-            <td colspan="6" class="text-center">No Agent Data</td>
-        </tr>
-        @else
-        @foreach($datas as $k => $v)
-        <tr>
-            <td>
-                <a href="">#{{ $v->id }}</a>
-            </td>
-            <td>{{ $v->first_name }}</td>
-            <td>{{ $v->last_name }}</td>
-            <td>{{ $v->email }}</td>
-            <td>{{ $v->phone }}</td>
-            <td></td>
-        </tr>
-        @endforeach
-        @endif
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Store Name</th>
+                <th>Open Time</th>
+                <th>Subscription</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if(count($datas) == 0)
+            <tr>
+                <td colspan="6" class="text-center">No Agent Data</td>
+            </tr>
+            @else
+            @foreach($datas as $k => $v)
+            <tr>
+                <td>
+                    @if($v->agent)
+                    <a href="javascript:void(0)">#{{ $v->agent['partner_id'] }}</a>
+                    @else
+                    <a href="javascript:void(0)">#{{ $v->id }}</a>
+                    @endif
+                </td>
+                <td>
+                    @if($v['agent'])
+                    {{ $v['agent']->store_name }}
+                    @else
+                    {{ $v->first_name }} {{ $v->last_name }}
+                    @endif
+                </td>
+                <td>
+                    @if($v['agent'])
+                    {{ $v['agent']->store_open }}-{{ $v['agent']->store_close }}
+                    @endif
+                </td>
+                <td>
+                    @if($v['agent'])
+                    <span class="badge badge-primary">{{ $v['agent']->subscription }}</span>
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+            @endif
+        </tbody>
     </table>
-
+    
     @if(count($datas) > 0)
     {!!  $datas->links() !!}
     @endif
